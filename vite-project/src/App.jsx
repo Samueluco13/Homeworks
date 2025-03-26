44ab954bfd9345f1be147ea9da19876020f95281
Samuel <sepulvedacsamuel09@gmail.com>
2025-03-26 16:36:38 -0500

    Primer avance challenge 8

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,36 +1,54 @@
-import { useState } from 'react'
-import './App.css'
+import React, {useState} from 'react'
+import {Book, BookStack} from "./Classes/Books"
 
+export const App = () => {
 
+    const PilaLibros = new BookStack();
 
+    //Agregando mocked data
+    PilaLibros.push(new Book("100 años de soledad", "12345678", "Alfaguara", "Gabriel García Márquez"))
+    PilaLibros.push(new Book("Padre Rico, Padre Pobre", "87654321", "Aguilar", "Libro de Robert Kiyosaki"))
+    PilaLibros.push(new Book("El cuervo", "91836614", "Editorial Alma", "Edgar Allan Poe"))
 
-function App({defindo}) {
-  const [contador, setContador] = useState(0)
+    const [libros, setLibros] = useState(PilaLibros.getBooks()) //Como default del estado se pone la pila con la mocked data
 
-  //FUNCIONES DE LOS BOTONES
-  const handleAdd = () => {
-    setContador(contador + 1)
-  }
+    const handleAdd = (e) => {
+        e.preventDefault(); //evita la recarga de la pagina
 
-const handleSubstract = () => {
-    setContador(contador - 1)
-  }
+        PilaLibros.push(new Book(libros.name = document.getElementsByName('name')[0].value,
+        libros.isbn = document.getElementsByName('isbn')[0].value,
+        libros.author = document.getElementsByName('author')[0].value,
+        libros.editorial = document.getElementsByName('editorial')[0].value))
 
-const handleReset = () => {
-    setContador(defindo)
-  }
-
-  return (
+        setLibros([...PilaLibros.getBooks()])
+        console.log(PilaLibros.getBooks())
+    }
+    
+    return (
     <>
-      <h1>CONTADOR</h1>
-      <div>
-        <button onClick={handleAdd}>Sumar</button>
-        <button onClick={handleSubstract}>Restar</button>
-        <button onClick={handleReset}>Reset</button>
-      </div>
-      <span>{contador}</span>
-    </>
-  )
-}
+    <div>PILA DE LIBROS</div>
+        <form action="">
+            <input name = 'name' type="text" placeholder = 'Nombre del libro'/>
+            <input name = 'isbn' type="text" placeholder = 'ISBN del libro'/>
+            <input name = 'author' type="text" placeholder = 'Autor del libro'/>
+            <input name = 'editorial' type="text" placeholder = 'Editorial del libro'/>
 
-export default App
+            <button onClick = {handleAdd}>Agregar libro</button>
+        </form>
+
+        <aside>
+            <h3>Lista de libros</h3>
+            <ul>
+                {
+                    libros.map(libro => {
+                        console.log(libro)
+                        return (<li key = {libro.isbn}>
+                            {libro.name}
+                        </li>)
+                    })
+                }
+            </ul>
+        </aside>
+    </>
+)
+}
