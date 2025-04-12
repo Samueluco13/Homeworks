346a7c049ed524cdab2c9120fd1862eecc2936f4
Samuel <sepulvedacsamuel09@gmail.com>
2025-04-12 18:33:24 -0500

    Se agrega un plus al challenge 11

--- vite-project/src/Slices/Thunks/googleAuth.js
+++ vite-project/src/Slices/Thunks/googleAuth.js
@@ -1,16 +1,15 @@
-// src/Slices/Thunks/loginWithGoogle.js
 import { signInWithPopup } from 'firebase/auth'
 import { auth, provider } from '../../firebase/config'
-import { login } from '../authSlice'
+import { setUser } from '../authSlice'
 
 export const googleAuth = () => {
     return async (dispatch) => {
     try {
         const result = await signInWithPopup(auth, provider)
 
-        const {email} = result.user
-        console.log(email)
-        dispatch(login({email}))
+        const {displayName, email} = result.user
+        console.log(result.user)
+        dispatch(setUser({displayName, email}))
 
     } catch (error) {
         console.error(" Error al iniciar sesión con Google:", error)
--- vite-project/src/Slices/Thunks/registerAuth.js
+++ vite-project/src/Slices/Thunks/registerAuth.js
@@ -1,17 +1,19 @@
 import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
 import { auth } from '../../firebase/config'
-import { register } from '../authSlice'
+import { setUser } from '../authSlice'
 
-export const registerAuth = (email, password) => {
+export const registerAuth = (displayname, email, password) => {
     return async (dispatch) =>{
         const response = await createUserWithEmailAndPassword(auth, email, password) //Espera a que firebase cree correctamente el usuario
+        let displayName = displayname //Crea una variable con el displayName que se le pasa como parametro
         if (response){
             await updateProfile(auth.currentUser, { //Actualiza el perfil que se acaba de crear con un username y una pfp
-                displayName: "Usuario1",
+                displayName, //Settea el displayName con la variable antes creada
                 photoURL: "https://unavatar.io/user1"
             })
+            console.log(auth.currentUser) //Muestra el usuario en consola
             const {email} = response.user; //Toma el correo del ususario recien creado
-            dispatch(register({email})) //Despacha la acción de redux "register" tomando como parametro el correo antes tomado
+            dispatch(setUser({email})) //Despacha la acción de redux "register" tomando como parametro el correo antes tomado
         }else{
             console.log("Error al registrar")
             throw new Error("Register failed") //Manejo de error
