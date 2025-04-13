965db2b021a4e1f1bfae792fe465d41d618ff640
Samuel <sepulvedacsamuel09@gmail.com>
2025-04-13 12:47:52 -0500

    Se sube el challenge 13

--- vite-project/src/Components/DataComponent.jsx
+++ vite-project/src/Components/DataComponent.jsx
@@ -1,8 +1,12 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import {useDispatch, useSelector } from "react-redux"
 import { fetchFirebaseData, addDataToFirebase } from '../Slices/Thunks/firebaseThunks'
+import { Link } from 'react-router-dom'
+import "../DataComponent.css"
 
 export const DataComponent = () => {
+    const [message, setMessage] = useState("");
+
     const dispatch = useDispatch();
     const {data, loading} = useSelector((state) => state.firebase)
 
@@ -11,23 +15,37 @@ export const DataComponent = () => {
     }, [dispatch]);
 
     const handleAddData = () => {
-        const newData = {name: "Nuevo Dato", value: Math.random()};
+        if (message.trim() === "") return alert("El mensaje debe tener contenido");
+
+        const newData = {value: message.trim()};
         dispatch(addDataToFirebase(newData));
+        setMessage("");
     }
     
     return (
-    <div>
-        <h2>Datos en Firebase</h2>
-        {
-            loading ? (<p>Crgando...</p>) : (
-                data.map((item, index) => (
-                    <p key={index}>
-                        {item.name}: {item.value}
-                    </p>
-                ))
-            )
-        }
-        <button onClick={handleAddData} >Agregar Dato</button>
+    <div className='chat-container'>
+        <p><Link to={"/"} >Volver al home</Link></p>
+        <h1 className='chat-title'>Datos en Firebase</h1>
+        <input
+        className='input-chat'
+        type="text"
+        name="message"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        placeholder="Ingrese un mensjae"
+        />
+        <button className='btn-chat' onClick={handleAddData} >Agregar mensjae</button>
+        <div className='chat-box'>
+            {
+                loading ? (<p className='chat-text'>Crgando...</p>) : (
+                    data.map((item, index) => (
+                        <p key={index} className='chat-text'>
+                            Mensaje #{index + 1}: {item.value}
+                        </p>
+                    ))
+                )
+            }
+        </div>
     </div>
     )
 }
